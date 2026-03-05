abd75b73cb2f015b5474915373fe2028589c892a
leviferre1ra <leviferreira1677@gmail.com>
2026-03-05 12:10:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logopoupapp.svg'
+
+function App() {
+
+  return (
+    <div className="bg-neutral-background min-h-screen ">
+      <aside>
+        <img src={logo} alt="" />
+      </aside>
+    </div>
+  )
+}
+
+export default App
